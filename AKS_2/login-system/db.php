d91823ec99c061e5dfcedd258f55cf7f3c9c997c
Geoff Sher <gsher2512@gmail.com>
2018-06-22 15:18:06 -0400

    cart

--- AKS_2/login-system/db.php
+++ AKS_2/login-system/db.php
@@ -7,8 +7,6 @@ $db = 'accounts';
 //switched to class because i felt like it was easier to work with than the array.
 $mysqli = mysqli_connect($host,$user,$pass,$db);
 
-
-
 if ($mysqli->connect_errno) 
 {
 	printf("Connect failed: %s\n", $mysqli->connect_error);
--- Cart/cart.php
+++ Cart/cart.php
@@ -1,40 +1,77 @@
 <?php
 session_start();
 $product_ids = array();
-//session_destroy();
+$discount_basket = array();
+$no_discount = array();
+function checkForDiscount($value, $key){
+		echo "Key is ".$key."<br>";
+	echo "Value is ".$value."<br>";
+
+	if($key == 'id'){
+		if ($value == 2 || $value == 4 || $value == 6){
+			echo "This basket will apply a discount to smaller baskets<br>";
+			//array_merge($discount_basket);
+		}
+		
+		else {
+			echo "This basket will NOT apply a discount to smaller baskets<br>";
+			//array_merge($value,$no_discount);
+		}
+}
+echo "<br>";
+
+}
+function apply_discount($product){
+	array_search($product);
+	
+}
 
 //check if Add to Cart button has been submitted
 if(filter_input(INPUT_POST, 'add_to_cart')){
+
     if(isset($_SESSION['shopping_cart'])){
         
-        //keep track of how mnay products are in the shopping cart
+        //keep track of how many products are in the shopping cart
         $count = count($_SESSION['shopping_cart']);
-        
+
         //create sequantial array for matching array keys to products id's
         $product_ids = array_column($_SESSION['shopping_cart'], 'id');
-        
+
+
+
+}
+
         if (!in_array(filter_input(INPUT_GET, 'id'), $product_ids)){
+
         $_SESSION['shopping_cart'][$count] = array
             (
                 'id' => filter_input(INPUT_GET, 'id'),
                 'name' => filter_input(INPUT_POST, 'name'),
                 'price' => filter_input(INPUT_POST, 'price'),
                 'quantity' => filter_input(INPUT_POST, 'quantity')
+
             );   
+
         }
+
         else { //product already exists, increase quantity
             //match array key to id of the product being added to the cart
-            for ($i = 0; $i < count($product_ids); $i++){
+
+            for ($i = 0; $i < count($product_ids); $i++){														//
                 if ($product_ids[$i] == filter_input(INPUT_GET, 'id')){
                     //add item quantity to the existing product in the array
                     $_SESSION['shopping_cart'][$i]['quantity'] += filter_input(INPUT_POST, 'quantity');
                 }
+
             }
+
         }
-        
+
+
     }
     else { //if shopping cart doesn't exist, create first product with array key 0
         //create array using submitted form data, start from key 0 and fill it with values
+
         $_SESSION['shopping_cart'][0] = array
         (
             'id' => filter_input(INPUT_GET, 'id'),
@@ -42,8 +79,9 @@ if(filter_input(INPUT_POST, 'add_to_cart')){
             'price' => filter_input(INPUT_POST, 'price'),
             'quantity' => filter_input(INPUT_POST, 'quantity')
         );
+
     }
-}
+
 
 if(filter_input(INPUT_GET, 'action') == 'delete'){
     //loop through all products in the shopping cart until it matches with GET id variable
@@ -51,6 +89,8 @@ if(filter_input(INPUT_GET, 'action') == 'delete'){
         if ($product['id'] == filter_input(INPUT_GET, 'id')){
             //remove product from the shopping cart when it matches with the GET id
             unset($_SESSION['shopping_cart'][$key]);
+									
+
         }
     }
     //reset session array keys so they match with $product_ids numeric array
@@ -65,37 +105,29 @@ function pre_r($array){
     echo '</pre>';
 }
 ?>
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>Shopping Cart</title>
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
-        <link rel="stylesheet" href="cart.css" />
+	<!DOCTYPE html>
+	<html>
 
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-    </head>
-	<style>
-	.banner {
-		margin: 1%;
-		text-align: center;
-		font-size: 2em;
-		border-style: solid;
-		border-width: 2px;
-		margin-left: auto;
-		margin-right: auto;
-		width: 90%;
-		padding: 10px;
-	}
-	</style>
-    <body>
+	<head>
+		<title>Shopping Cart</title>
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
+		<link rel="stylesheet" href="cart.css" />
 
-        <div class="container-fluid">
-        <?php
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+	</head>
+	
+	<body>
 
+		<div class="container-fluid">
+			<?php
 
+		//enter connection information on line 100
+		//$connect = mysqli_connect('localhost', 'username', 'password' ,'databasename');
         $connect = mysqli_connect('localhost', 'root', '' ,'cart');
+		
+		//remove the table each time since this is a test
 		$sql = "DROP TABLE products";
 		$connect->query($sql);
 
@@ -103,12 +135,12 @@ function pre_r($array){
 		$connect->query($sql);
 
 		$sql = "INSERT INTO products(id, name, price, image) VALUES
-		('1', 'Small Basket with Apples', '19.99', 'basket1.jpg'),
-		(2, 'Small Basket', 18.99, 'basket4.jpg'),
-		(3, 'Medium Basket with Apples', 24.99, 'basket1.jpg'),
-		(4, 'Medium Basket', 23.99, 'basket4.jpg'),
-		(5, 'Large Basket with Apples', 29.99, 'basket1.jpg'),
-		(6, 'Large Basket', 28.99, 'basket4.jpg')";
+		('2', 'Small Basket with Apples', '19.99', 'basket1.jpg'),
+		('1', 'Small Basket', 18.99, 'basket4.jpg'),
+		('4', 'Medium Basket with Apples', 24.99, 'basket1.jpg'),
+		('3', 'Medium Basket', 23.99, 'basket4.jpg'),
+		('6', 'Large Basket with Apples', 29.99, 'basket1.jpg'),
+		('5', 'Large Basket', 28.99, 'basket4.jpg')";
 		$connect->query($sql);
 		
         $query = 'SELECT * FROM products ORDER by id ASC';
@@ -118,106 +150,144 @@ function pre_r($array){
             if(mysqli_num_rows($result)>0):
                 while($product = mysqli_fetch_assoc($result)):
                 ?>
-                <div class="col-sm-4 col-md-3" >
-                    <form method="post" action="cart.php?action=add&id=<?php echo $product['id']; ?>">
-                        <div class="products">
-                            <img src="<?php echo $product['image']; ?>" class="img-responsive" />
-                            <h4 class="text-info"><?php echo $product['name']; ?></h4>
-                            <h4>$ <?php echo $product['price']; ?></h4>
-                            <input type="text" name="quantity" class="form-control" value="1" />
-                            <input type="hidden" name="name" value="<?php echo $product['name']; ?>" />
-                            <input type="hidden" name="price" value="<?php echo $product['price']; ?>" />
-                            <input type="submit" name="add_to_cart" style="margin-top:5px;" class="btn btn-info"
-                                   value="Add to Cart" />
-                        </div>
-                    </form>
-                </div>
-                <?php
+				<div class="col-sm-4 col-md-3">
+					<form method="post" action="cart.php?action=add&id=<?php echo $product['id']; ?>">
+						<div class="products">
+							<img src="<?php echo $product['image']; ?>" class="img-responsive" />
+							<h4 class="text-info">
+								<?php echo $product['name']; ?>
+							</h4>
+							<h4>$
+								<?php echo $product['price']; ?>
+							</h4>
+							<input type="text" name="quantity" class="form-control" value="1" />
+							<input type="hidden" name="name" value="<?php echo $product['name']; ?>" />
+							<input type="hidden" name="price" value="<?php echo $product['price']; ?>" />
+							<input type="submit" name="add_to_cart" style="margin-top:5px;" class="btn btn-info" value="Add to Cart" />
+						</div>
+					</form>
+				</div>
+				<?php
                 endwhile;
             endif;
         endif;   
         ?>
-		
-        <div style="clear:both"></div>  
-			<div class="banner">
-			<div>
-				Enter the promo code
-				<strong style="color:green">APPLE50</strong> to save 50% on a second basket at checkout<br> when you order any basket containing apples.
-				<br>
-				<font size="2">*Discount applies to items of equal or lesser value.</font>
-			</div>
-			</div>
-        <br />  
-        <div class="table-responsive">  
-        <table class="table">  
-            <tr><th colspan="5"><h3>Order Details</h3></th></tr>   
-        <tr>  
-             <th width="40%">Product Name</th>  
-             <th width="10%">Quantity</th>  
-             <th width="20%">Price</th>  
-             <th width="15%">Total</th>  
-             <th width="5%">Action</th>  
-        </tr>  
-        <?php   
-        if(!empty($_SESSION['shopping_cart'])):  
-            
-             $total = 0;  
-        
-             foreach($_SESSION['shopping_cart'] as $key => $product): 
-        ?>  
-        <tr>  
-           <td><?php echo $product['name']; ?></td>  
-           <td><?php echo $product['quantity']; ?></td>  
-           <td>$ <?php echo $product['price']; ?></td>  
-           <td>$ <?php echo number_format($product['quantity'] * $product['price'], 2); ?></td>  
-           <td>
-               <a href="cart.php?action=delete&id=<?php echo $product['id']; ?>">
-                    <div class="btn-danger">Remove</div>
-               </a>
-           </td>  
-        </tr>  
-        <?php  
-                  $total = $total + ($product['quantity'] * $product['price']);  
-             endforeach;  
-        ?>  
-					<?php $discountString = $_GET['discountcode'];?>
 
-        <tr>  
-             <td colspan="3" align="right">Total</td>  
-             <td align="left"><strong>$ <?php echo number_format($total, 2); ?></strong></td>  
-             <td></td>  
-        </tr> 
-		<tr>
-			 <td colspan="4" align="right"><form method="GET">Enter Discount Code:<input type ="text" id="discountcode" name="discountcode"></form></td>
-			<td><a href="#" class="btn-success">Apply</a></td>
+			<div style="clear:both"></div>
+<!-- 					<div class="banner">
+						<div>
+							Enter the promo code
+							<strong style="color:green">APPLE50</strong> to save 50% on a second basket at checkout
+							<br> when you order any basket containing apples.
+							<br>
+							<font size="2">*Discount applies to items of equal or lesser value.</font>
+						</div>
+					</div> -->
+			<br />
+			<div class="table-responsive">
+				<table class="table">
+					<tr>
+						<th colspan="5">
+							<h3>Order Details</h3>
+						</th>
+					</tr>
+					<tr>
+                        <th width="40%">Product Name</th>
+                        <th width="10%">Quantity</th>
+						<th width="20%">Price</th>
+						<th width="15%">Total</th>
+						<th width="5%">Action</th>
+					</tr>
+		<?php   
+            if(!empty($_SESSION['shopping_cart'])):  
+                
+                $total = 0;  
+                
+                foreach($_SESSION['shopping_cart'] as $key => $product): 
+                print_r(array_values($product));
+                echo "<br>";
+                //array_walk_recursive($product, "checkForDiscount");
+                echo "<br>";
+                array_push($discount_basket, $product);
+        ?>
+					<tr>
+						<td>
+							<?php echo $product['name']; ?>
+						</td>
+						<td>
+							<?php echo $product['quantity']; ?>
+						</td>
+						<td>$
+							<?php echo $product['price']; ?>
+						</td>
+						<td>$
+							<?php echo number_format($product['quantity'] * $product['price'], 2); ?>
+						</td>
+						<td>
+							<a href="cart.php?action=delete&id=<?php echo $product['id']; ?>">
+								<div class="btn-danger">Remove</div>
+							</a>
+						</td>
+					</tr>
+		<?php  
+                    $total = $total + ($product['quantity'] * $product['price']);  
+                endforeach; 
+			    print_r(array_values($discount_basket));
+			    echo "<br><br>";
+			    array_walk_recursive($discount_basket, "checkForDiscount");
 
-</td>
- 
-</tr>		
-        <tr>
-            <!-- Show checkout button only if the shopping cart is not empty -->
-            <td colspan="5" align="right">
-             <?php 
+        ?>
+
+					<tr>
+						<td colspan="3" align="right">Total</td>
+						<td align="left">
+							<strong>$
+								<?php echo number_format($total, 2); ?>
+							</strong>
+						</td>
+						<td></td>
+					</tr>
+<!-- 							<tr>
+								<td colspan="4" align="right">
+									<form method="POST">Enter Discount Code:
+										<input type="text" id="discountcode" name="discountcode">
+									</form>
+								</td>
+								<td>
+									<a href="applydiscount.php" class="btn-success">Apply</a>
+								</td>
+
+								</td>
+
+							</tr> */
+							<tr>
+-->
+								<!-- Show checkout button only if the shopping cart is not empty -->
+
+						<td colspan="5" align="right">
+			<?php 
                 if (isset($_SESSION['shopping_cart'])):
                 if (count($_SESSION['shopping_cart']) > 0):
-             ?>
+            ?>
 
-                <a href="applydiscount()" class="button">Checkout</a>
-             <?php endif; endif; ?>
-            </td>
-        </tr>
-        <?php  
+							<a href="#"class="button"  name="checkout" >Checkout</a>
+							<?php endif; endif; ?>
+						</td>
+					</tr>
+					<?php  
         endif;
-		
-		function applydiscount($discountString){
-			if ($discountString == 'APPLY50'){
-			echo "Discount Applied";}
-			else {
-			echo "incorrect code";}
-		}
-        ?>  
-        </table>  
-         </div>
-        </div>
-    </body>
-</html>
+
+    
+
+        ?>
+						</table>
+						
+					</div>
+		</div>
+<!-- TEST PHP ARRAY CONTENTS -->
+<?php
+
+?>
+	</body>
+
+	</html>
